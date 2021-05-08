372ff6c77794c989e4f2179cfe01d33f2658188a
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2021-05-08 15:40:16 -0300

    feat: Código compartilhado no diretório shared

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+// import api from '@monorepo/axios-config'
 
 const App: React.FC = () => {
   return <h1>Hello World</h1>
 }
 
-export default App;
+export default App
